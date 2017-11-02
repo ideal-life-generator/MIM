9a9b257f15da17d0dfa32dab2e1e35527afe101e
vlad2018 <vlad2018@LearniOSFreecoms-Mac.local>
2017-11-02 00:06:00 +0000

    hm

--- MIM/ViewController.swift
+++ MIM/ViewController.swift
@@ -1,25 +1,50 @@
 //
-//  ViewController.swift
+//  SearchController.swift
 //  MIM
 //
-//  Created by vlad2018 on 10/25/17.
+//  Created by vlad2018 on 10/29/17.
 //  Copyright © 2017 ideallifegenerator. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  var cachedSongs = [CachedSong]()
+  
+  @IBOutlet weak var searchTextField: UITextField!
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var tableHeight: NSLayoutConstraint!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    cachedSongs += [
+      CachedSong(name: "First"),
+      CachedSong(name: "Second"),
+      CachedSong(name: "Third"),
+      CachedSong(name: "Fourth"),
+    ]
+  }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  @IBAction func onSearchTextFieldChange(_ textField: UITextField) {
+    print(textField.text ?? "")
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return cachedSongs.count
+  }
 
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "CachedSongCell")
 
+    cell.textLabel?.text = cachedSongs[indexPath.row].name
+
+    return cell
+  }
+  
+  override func viewWillLayoutSubviews() {
+    super.updateViewConstraints()
+
+    tableHeight?.constant = tableView.contentSize.height
+  }
 }
-
